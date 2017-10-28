2daba1eff7ba3e9a91bdf96dff687bea66e583da
Xie Guoyong <735332492@qq.com>
2017-10-29 00:11:47 +0800

    Head First Java Chap17 example

---
+++ head_first_java/chap17/eg1/source/TestSyncTest.java
@@ -0,0 +1,25 @@
+
+class TestSync implements Runnable{
+    private int balance;
+    public void run(){
+        for (int i =0;i<50;i++){
+            increament();
+            System.out.println("balance is : " + balance);
+        }
+    }
+    public synchronized void increament(){
+        int i = balance;
+        balance = i+1;
+    }
+}
+
+public class TestSyncTest{
+    public static void main(String[] args){
+        TestSync work = new TestSync();
+        Thread a = new Thread(work);
+        Thread b = new Thread(work);
+
+        a.start();
+        b.start();
+    }
+}
